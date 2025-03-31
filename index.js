365b188487420ea49264a9538447040961464d98
Eugene Kapombe <kapombek@gmail.com>
2025-03-31 15:09:20 +0300

    fixed function name

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 function receivesAFunction(callback) {
-    return callback();
+  return callback();
 }
 
 function returnsANamedFunction() {
-    return function named() {
-        console.log("I have a name.")
-    }
+  return function named() {
+    console.log("I have a name.");
+  };
 }
 
-function returnsAnonymousFunction () {
-    return function() {
-        console.log("I have no name.")
-    } 
+function returnsAnAnonymousFunction() {
+  return function() {
+    console.log("I have no name.");
+  };
 }
